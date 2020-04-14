92bb32dd8bc14065fd15009f823fb82ba2dff3d3
MariaDor <dorofeeva.m.01@gmail.com>
2020-04-14 18:20:04 +0300

    pr6.1

--- pr6/script.js
+++ pr6/script.js
@@ -1,4 +1,32 @@
 function arrays(n) {
     let array1 = [];
     let array2 = [];
+    for (let i = 1; i <= n; i++) {
+        array1.push(Math.floor(Math.random() * 100));
+    }
+    array2 = array1.filter((el) => el % 2 == 1)
+    console.log(array1.join(', '));
+    console.log(array2.join(', '));
+}
+
+function insertArray(n) {
+    let array1 = [];
+    let array2 = [];
+    for (let i = 1; i <= n; i++) {
+        array1.push(Math.floor(Math.random() * 100));
+    }
+    inputArray(array1)
+    for (let i = 1; i <= n; i++) {
+        array2.push(Math.floor(Math.random() * 100));
+    }
+    inputArray(array2)
+    array1 = array1.concat(array2);
+    inputArray(array1);
+}
+
+function inputArray(array) {
+    console.log("Array:")
+    array.forEach((el, i) => {
+        console.log("el"+i+" = "+el);
+    });
 }

ed6c090d48fc8ea31ece9ad4744f0f82d24bf302
MariaDor <dorofeeva.m.01@gmail.com>
2020-04-16 09:29:13 +0300

    pr10

--- pr8/script.js
+++ pr8/script.js
@@ -18,8 +18,8 @@ function sentenceDivision(str) {
     }
 }
 
-// wordDivision('ар в-ыдш адFв4ы 4 Dоао gjdk-D f')
-// sentenceDivision("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nisi neque, malesuada id feugiat pharetra, molestie vitae est. Suspendisse tincidunt ligula dui, nec egestas quam laoreet sed. Suspendisse ac nisi eu sapien rhoncus scelerisque non vitae ante. Nullam tortor orci, dapibus a placerat in, facilisis non nulla. Nam nec nulla eu mi iaculis scelerisque!? Sed laoreet neque ac ipsum interdum consectetur. Morbi nec ornare lacus, sed rutrum risus? Etiam nec eleifend quam... Pellentesque volutpat nibh nec rutrum finibus! Sed turpis dolor, lacinia sit amet arcu sit amet, dictum egestas arcu. Etiam sapien massa, malesuada ut sagittis sagittis, mollis sit amet sapien. Curabitur sit amet massa turpis.")
+wordDivision('ар в-ыдш адFв4ы 4 Dоао gjdk-D f')
+sentenceDivision("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nisi neque, malesuada id feugiat pharetra, molestie vitae est. Suspendisse tincidunt ligula dui, nec egestas quam laoreet sed. Suspendisse ac nisi eu sapien rhoncus scelerisque non vitae ante. Nullam tortor orci, dapibus a placerat in, facilisis non nulla. Nam nec nulla eu mi iaculis scelerisque!? Sed laoreet neque ac ipsum interdum consectetur. Morbi nec ornare lacus, sed rutrum risus? Etiam nec eleifend quam... Pellentesque volutpat nibh nec rutrum finibus! Sed turpis dolor, lacinia sit amet arcu sit amet, dictum egestas arcu. Etiam sapien massa, malesuada ut sagittis sagittis, mollis sit amet sapien. Curabitur sit amet massa turpis.")
 
 //2
 function passwordCheck(password) {
